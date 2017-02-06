442d0c03ff35bf44cd9ac066b62097d39508151d
laiyanlong <laiyanlong@gmail.com>
2017-02-07 01:17:41 +0800

    add time ago and text limit

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,8 @@
-var category = require('../models/category');
-var rssFeed = require('../models/rssFeed');
-var rss = require('../models/rss');
+const moment = require('moment');
+
+const category = require('../models/category');
+const rssFeed = require('../models/rssFeed');
+const rss = require('../models/rss');
 
 category.readAll().then(function(result){
     // console.log('!!!', result.rowCount);
@@ -10,15 +12,23 @@ rssFeed.readAll().then(function(result){
     // console.log('???', result);
 })
 
+const URL_LENGTH = 60
 
 /**
  * GET /
  * Home page.
  */
 exports.index = (req, res) => {
-
   rss.readAll(20, 3).then(function(result){
-    console.log('+++', result)
+    result.rows.forEach(function(item) {
+      // change time format
+      item.timeago = moment(item.createtimestamp).fromNow()
+      if (item.rssurl.length > URL_LENGTH) {
+        item.trimRssurl = item.rssurl.substring(0, URL_LENGTH - 3) + '...'
+      } else {
+        item.trimRssurl = item.rssurl
+      }
+    })
     res.render('home', {
       title: 'Welcome to Artifactblue',
       data: [1, 2, 3, 4],
