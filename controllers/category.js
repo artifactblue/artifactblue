b13b92cff1f99dd697ef706d04ec2fd5fcf60641
laiyanlong <laiyanlong@gmail.com>
2017-02-07 23:12:09 +0800

    update level 2 to get category rss list

--- controllers/category.js
+++ controllers/category.js
@@ -1,7 +1,12 @@
+const moment = require('moment')
 // var category = require('../models/category');
 // var rssFeed = require('../models/rssFeed');
-// var rss = require('../models/rss');
+const rss = require('../models/rss')
 
+const RSS_LIMIT = 40
+const RSS_OFFSET = 0
+
+const URL_LENGTH = 60
 // category.readAll().then(function(result){
 //     // console.log('!!!', result.rowCount);
 // })
@@ -10,16 +15,27 @@
 //     console.log('???', result);
 // })
 
-// rss.readAll(2, 3, 0).then(function(result){
-//     // console.log('+++', result);
-// })
 /**
- * GET /
- * Home page.
+ * GET /i/categoryId/
+ * category page.
  */
 exports.index = (req, res) => {
-  res.render('category', {
-    title: 'category',
-    categoryId: req.params.categoryId,
-  });
-};
+  rss.readByCategoryId(req.params.categoryId, RSS_LIMIT, RSS_OFFSET).then(
+    result => {
+      result.rows.forEach( item => {
+        // console.log('item', item)
+        // change time format
+        item.timeago = moment(item.createtimestamp).fromNow()
+        if (item.rssurl.length > URL_LENGTH) {
+          item.trimRssurl = item.rssurl.substring(0, URL_LENGTH - 3) + '...'
+        } else {
+          item.trimRssurl = item.rssurl
+        }
+      })
+      res.render('category', {
+        title: 'category',
+        categoryId: req.params.categoryId,
+        result: result.rows,
+      })
+    })
+}
