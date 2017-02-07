939ecfbc4d8349d0c5f1e77f12b40c0b5c9afba6
laiyanlong <laiyanlong@gmail.com>
2017-02-07 22:52:03 +0800

    classify by category in home page

--- controllers/home.js
+++ controllers/home.js
@@ -4,13 +4,29 @@ const category = require('../models/category')
 const rssFeed = require('../models/rssFeed')
 const rss = require('../models/rss')
 
-category.readAll().then(function(result){
-    // console.log('!!!', result.rowCount)
-})
+const CATEGORY_LIMIT = 10
+const CATEGORY_OFFSET = 1
+const CATEGORY_ARRAY = [1, 2, 3, 4, 5]
 
-rssFeed.readAll().then(function(result){
-    // console.log('???', result)
-})
+const RSS_LIMIT = 40
+const RSS_OFFSET = 0
+
+// rssFeed.readAll().then(
+//   function(result){
+//     // console.log('???', result)
+// })
+
+// category.readAll(
+//   CATEGORY_LIMIT,
+//   CATEGORY_OFFSET
+//   ).then(
+//     result => {
+//       // console.log('!!!', result.rows)
+//       result.rows.forEach( item => {
+
+//       })
+//     }
+//   )
 
 const URL_LENGTH = 60
 
@@ -19,20 +35,24 @@ const URL_LENGTH = 60
  * Home page.
  */
 exports.index = (req, res) => {
-  rss.readAll(20, 3).then(function(result){
-    result.rows.forEach(function(item) {
-      // change time format
-      item.timeago = moment(item.createtimestamp).fromNow()
-      if (item.rssurl.length > URL_LENGTH) {
-        item.trimRssurl = item.rssurl.substring(0, URL_LENGTH - 3) + '...'
-      } else {
-        item.trimRssurl = item.rssurl
-      }
-    })
-    res.render('home', {
-      title: 'Welcome to Artifactblue',
-      data: [1, 2, 3, 4],
-      result: result.rows,
-    })
-  })
+
+  rss.readAll(RSS_LIMIT, RSS_OFFSET).then(
+    result => {
+      result.rows.forEach( item => {
+        console.log('item', item)
+        // change time format
+        item.timeago = moment(item.createtimestamp).fromNow()
+        if (item.rssurl.length > URL_LENGTH) {
+          item.trimRssurl = item.rssurl.substring(0, URL_LENGTH - 3) + '...'
+        } else {
+          item.trimRssurl = item.rssurl
+        }
+      })
+      res.render('home', {
+        title: 'Welcome to Artifactblue',
+        categoryArray: CATEGORY_ARRAY,
+        result: result.rows,
+      })
+    }
+  )
 }
