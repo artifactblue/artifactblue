3b18c5e2d81899b58bbed4ada0c80db3b5b73dd2
laiyanlong <laiyanlong@gmail.com>
2017-02-18 00:39:19 +0800

    add parser in feed page

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,6 +3,9 @@ const rssFeed = require('../models/rssFeed')
 const rss = require('../models/rss')
 const category = require('../models/category')
 
+// crawer feed page
+const MetaInspector = require('node-metainspector')
+
 const URL_LENGTH = 60
 // const RSS_LIMIT = 40
 
@@ -25,6 +28,23 @@ const getTrimThumbnail = thumbnail => {
   return thumbnail
 }
 
+const getFeedImages = (feedUrl) => {
+  const client = new MetaInspector(feedUrl, { timeout: 5000 })
+  client.on('fetch', function(){
+    const clientImages = client.images
+    var Images = []
+    for(var i = 0; i < clientImages.length ; i++ ) {
+      Images.push(clientImages[i])
+    }
+    // console.log(Images)
+    return Images
+  })
+  client.on('error', function(err){
+    console.log(err)
+  })
+  client.fetch()
+}
+
 /**
  * GET /
  * RSS feeds page.
@@ -44,17 +64,40 @@ exports.index = (req, res) => {
 
         const reImg = '/' + item.trimThumbnail + '/'
         const rssfeedJSON = JSON.parse(item.rssfeedcontent)
-        const rssfeedDescription = rssfeedJSON.description
+        var rssfeedDescription = rssfeedJSON.description
         const trimRssfeedDescription = rssfeedDescription.replace(reImg, '')
 
-        res.render('feed', {
-          categoryName: categoryResult.rows[0].name,
-          rssName: rssResult.rows[0].rssname,
-          categoryId: req.params.categoryId,
-          rssId: req.params.rssId,
-          rssfeedcontent: trimRssfeedDescription,
-          item,
+
+        var client = new MetaInspector(item.rssfeedurl, { timeout: 5000 })
+        client.on('fetch', function(){
+          const clientImages = client.images
+          var Images = []
+          for(var i = 0; i < clientImages.length ; i++ ) {
+            Images.push(clientImages[i])
+          }
+          item.feedImages = Images
+          res.render('feed', {
+            categoryName: categoryResult.rows[0].name,
+            rssName: rssResult.rows[0].rssname,
+            categoryId: req.params.categoryId,
+            rssId: req.params.rssId,
+            rssfeedcontent: trimRssfeedDescription,
+            item,
+          })
         })
+        client.on('error', function(err){
+          console.log(err)
+        })
+        client.fetch()
+
+        // var imagesArray = []
+        // const feedImages = getFeedImages(item.rssfeedurl)
+        // // for(var i = 0; i < feedImages.length ; i++ ) {
+        // //   imagesArray.push(feedImages[i])
+        // // }
+        // console.log('test', feedImages)
+        // item.feedImages = imagesArray
+
       })
     })
   })
