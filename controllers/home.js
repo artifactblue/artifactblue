55dc7691d90f580434c6ef8aaacf3638d4604dad
Yung chieh Tsai <x01ep23i@hotmail.com>
2017-02-24 12:08:24 +0800

    update home.js

--- controllers/home.js
+++ controllers/home.js
@@ -1,22 +1,29 @@
 const moment = require('moment')
 
-// const category = require('../models/category')
+const category = require('../models/category')
 // const rssFeed = require('../models/rssFeed')
 const rss = require('../models/rss')
 
-// const CATEGORY_LIMIT = 10
-// const CATEGORY_OFFSET = 1
-const CATEGORY_ARRAY = [1, 2, 3/*, 4, 5, 6, 7, 8*/]
-const CATEGORY_MAP = {
-  1: '資訊',
-  2: '正妹',
-  3: '生活',
+const CATEGORY_LIMIT = 10
+const CATEGORY_OFFSET = 0
+const CATEGORY_ARRAY = [/*1, 2, 3, 4, 5, 6, 7, 8*/]
+var CATEGORY_MAP = {
+  // 1: '資訊',
+  // 2: '正妹',
+  // 3: '生活',
   // 4: '研究',
   // 5: '資源',
   // 6: 'iphoneapp',
   // 7: '新聞',
   // 8: '其他議題',
 }
+
+category.readAll(CATEGORY_LIMIT, CATEGORY_OFFSET).then(function(categoryResult){
+  categoryResult.rows.forEach(function(categoryItem){
+    CATEGORY_MAP[categoryItem.id] = categoryItem.name
+    CATEGORY_ARRAY.push(categoryItem.id)
+  })
+})
 
 const RSS_LIMIT = 100
 const RSS_OFFSET = 0
